7ff76d641fbe1a06da18b00b052d2cdef59645d5
Chong Zhou <chongzhou1024@gmail.com>
2023-12-13 15:01:34 +0800

    add ONNX backend

---
+++ scripts/export_onnx_model.py
@@ -0,0 +1,121 @@
+import torch
+import argparse
+from segment_anything import sam_model_registry
+from segment_anything.utils.coreml import SamCoreMLModel
+
+
+parser = argparse.ArgumentParser(
+    description="Export the EdgeSAM to ONNX models."
+)
+
+parser.add_argument(
+    "checkpoint", type=str, help="The path to the EdgeSAM model checkpoint."
+)
+
+parser.add_argument(
+    "--gelu-approximate",
+    action="store_true",
+    help=(
+        "Replace GELU operations with approximations using tanh. Useful "
+        "for some runtimes that have slow or unimplemented erf ops, used in GELU."
+    ),
+)
+
+parser.add_argument(
+    "--use-stability-score",
+    action="store_true",
+    help=(
+        "Replaces the model's predicted mask quality score with the stability "
+        "score calculated on the low resolution masks using an offset of 1.0. "
+    ),
+)
+
+parser.add_argument(
+    "--decoder",
+    action="store_true",
+    help="If set, export decoder, otherwise export encoder",
+)
+
+
+def export_encoder_to_onnx(sam, args):
+    if args.gelu_approximate:
+        for n, m in sam.named_modules():
+            if isinstance(m, torch.nn.GELU):
+                m.approximate = "tanh"
+
+    image_input = torch.randn(1, 3, 1024, 1024, dtype=torch.float)
+    sam.forward = sam.forward_dummy_encoder
+
+    traced_model = torch.jit.trace(sam, image_input)
+
+    # Define the input names and output names
+    input_names = ["image"]
+    output_names = ["image_embeddings"]
+
+    # Export the encoder model to ONNX format
+    onnx_encoder_filename = args.checkpoint.replace('.pth', '_encoder.onnx')
+    torch.onnx.export(
+        traced_model,
+        image_input,
+        onnx_encoder_filename,
+        input_names=input_names,
+        output_names=output_names,
+        opset_version=11,  # Use an appropriate ONNX opset version
+        verbose=False
+    )
+
+    print(f"Exported ONNX encoder model to {onnx_encoder_filename}")
+
+
+def export_decoder_to_onnx(sam, args):
+    sam_decoder = SamCoreMLModel(
+        model=sam,
+        use_stability_score=args.use_stability_score
+    )
+    sam_decoder.eval()
+
+    if args.gelu_approximate:
+        for n, m in sam.named_modules():
+            if isinstance(m, torch.nn.GELU):
+                m.approximate = "tanh"
+
+    embed_dim = sam.prompt_encoder.embed_dim
+    embed_size = sam.prompt_encoder.image_embedding_size
+
+    image_embeddings = torch.randn(1, embed_dim, *embed_size, dtype=torch.float)
+    point_coords = torch.randint(low=0, high=1024, size=(1, 5, 2), dtype=torch.float)
+    point_labels = torch.randint(low=0, high=4, size=(1, 5), dtype=torch.float)
+
+    # Define the input names and output names
+    input_names = ["image_embeddings", "point_coords", "point_labels"]
+    output_names = ["scores", "masks"]
+
+    # Export the decoder model to ONNX format
+    onnx_decoder_filename = args.checkpoint.replace('.pth', '_decoder.onnx')
+    torch.onnx.export(
+        sam_decoder,
+        (image_embeddings, point_coords, point_labels),
+        onnx_decoder_filename,
+        input_names=input_names,
+        output_names=output_names,
+        opset_version=11,  # Use an appropriate ONNX opset version
+        dynamic_axes={
+            "point_coords": {1: "num_points"},
+            "point_labels": {1: "num_points"},
+        },
+        verbose=False
+    )
+
+    print(f"Exported ONNX decoder model to {onnx_decoder_filename}")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print("Loading model...")
+    sam = sam_model_registry["edge_sam"](checkpoint=args.checkpoint, upsample_mode="bilinear")
+    sam.eval()
+
+    if args.decoder:
+        export_decoder_to_onnx(sam, args)
+    else:
+        export_encoder_to_onnx(sam, args)
--- web_demo/gradio_app.py
+++ web_demo/gradio_app.py
@@ -1,9 +1,10 @@
 # Code credit: [FastSAM Demo](https://huggingface.co/spaces/An-619/FastSAM).
 
+import torch
 import gradio as gr
 import numpy as np
-import torch
-from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+from segment_anything import sam_model_registry, SamPredictor
+from segment_anything.onnx import SamPredictorONNX
 from PIL import ImageDraw
 from utils.tools_gradio import fast_process
 import copy
@@ -16,13 +17,24 @@ parser.add_argument(
     "--checkpoint",
     default="weights/edge_sam_3x.pth",
     type=str,
-    help="The path to the EdgeSAM model checkpoint."
+    help="The path to the PyTorch checkpoint of EdgeSAM."
 )
 parser.add_argument(
-    "--enable-everything-mode",
+    "--encoder-onnx-path",
+    default="weights/edge_sam_3x_encoder.onnx",
+    type=str,
+    help="The path to the ONNX model of EdgeSAM's encoder."
+)
+parser.add_argument(
+    "--decoder-onnx-path",
+    default="weights/edge_sam_3x_decoder.onnx",
+    type=str,
+    help="The path to the ONNX model of EdgeSAM's decoder."
+)
+parser.add_argument(
+    "--enable-onnx",
     action="store_true",
-    help="Since EdgeSAM follows the same encoder-decoder architecture as SAM, the everything mode will infer the "
-         "decoder 32x32=1024 times, which is inefficient, thus a longer processing time is expected.",
+    help="Use ONNX to speed up the inference.",
 )
 parser.add_argument(
     "--server-name",
@@ -39,12 +51,16 @@ parser.add_argument(
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-sam = sam_model_registry["edge_sam"](checkpoint=args.checkpoint, upsample_mode="bicubic")
-sam = sam.to(device=device)
-sam.eval()
+if args.enable_onnx:
+    # device = "cpu"
+    predictor = SamPredictorONNX(args.encoder_onnx_path, args.decoder_onnx_path)
+else:
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    sam = sam_model_registry["edge_sam"](checkpoint=args.checkpoint, upsample_mode="bicubic")
+    sam = sam.to(device=device)
+    sam.eval()
+    predictor = SamPredictor(sam)
 
-mask_generator = SamAutomaticMaskGenerator(sam)
-predictor = SamPredictor(sam)
 
 examples = [
     ["web_demo/assets/1.jpeg"],
@@ -87,16 +103,6 @@ description_b = """ # Instructions for box mode
 
               """
 
-description_e = """ # Everything mode is NOT recommended.
-
-                Since EdgeSAM follows the same encoder-decoder architecture as SAM, the everything mode will infer the decoder 32x32=1024 times, which is inefficient, thus a longer processing time is expected.
-
-                1. Upload an image or click one of the provided examples.
-                2. Click Start to get the segmentation mask.
-                3. The Reset button resets the image and masks.
-
-              """
-
 css = "h1 { text-align: center } .about { text-align: justify; padding-left: 10%; padding-right: 10%; }"
 
 global_points = []
@@ -131,10 +137,7 @@ def reset_all():
     global_box = []
     global_image = None
     global_image_with_prompt = None
-    if args.enable_everything_mode:
-        return None, None, None
-    else:
-        return None, None
+    return None, None
 
 
 def clear():
@@ -215,26 +218,30 @@ def segment_with_points(
     )
     image = global_image_with_prompt
 
-    global_points_np = np.array(global_points)
-    global_point_label_np = np.array(global_point_label)
-
-    num_multimask_outputs = 4
-
-    masks, scores, logits = predictor.predict(
-        point_coords=global_points_np,
-        point_labels=global_point_label_np,
-        num_multimask_outputs=num_multimask_outputs,
-        use_stability_score=True
-    )
+    if args.enable_onnx:
+        global_points_np = np.array(global_points)[None]
+        global_point_label_np = np.array(global_point_label)[None]
+        masks, scores, _ = predictor.predict(
+            point_coords=global_points_np,
+            point_labels=global_point_label_np,
+        )
+        masks = masks.squeeze(0)
+        scores = scores.squeeze(0)
+    else:
+        global_points_np = np.array(global_points)
+        global_point_label_np = np.array(global_point_label)
+        masks, scores, logits = predictor.predict(
+            point_coords=global_points_np,
+            point_labels=global_point_label_np,
+            num_multimask_outputs=4,
+            use_stability_score=True
+        )
 
     print(f'scores: {scores}')
     area = masks.sum(axis=(1, 2))
     print(f'area: {area}')
 
-    if num_multimask_outputs == 1:
-        annotations = masks
-    else:
-        annotations = np.expand_dims(masks[scores.argmax()], axis=0)
+    annotations = np.expand_dims(masks[scores.argmax()], axis=0)
 
     seg = fast_process(
         annotations=annotations,
@@ -294,12 +301,20 @@ def segment_with_box(
         )
 
         global_box_np = np.array(global_box)
-
-        masks, scores, logits = predictor.predict(
-            box=global_box_np,
-            num_multimask_outputs=1,
-        )
-        annotations = masks
+        if args.enable_onnx:
+            point_coords = global_box_np.reshape(2, 2)[None]
+            point_labels = np.array([2, 3])[None]
+            masks, _, _ = predictor.predict(
+                point_coords=point_coords,
+                point_labels=point_labels,
+            )
+            annotations = masks[:, 0, :, :]
+        else:
+            masks, scores, _ = predictor.predict(
+                box=global_box_np,
+                num_multimask_outputs=1,
+            )
+            annotations = masks
 
         seg = fast_process(
             annotations=annotations,
@@ -315,41 +330,8 @@ def segment_with_box(
         return seg
     return image
 
-
-def segment_everything(
-        image,
-        input_size=1024,
-        better_quality=False,
-        withContours=True,
-        use_retina=True,
-        mask_random_color=True,
-):
-    nd_image = np.array(image)
-    masks = mask_generator.generate(nd_image)
-    annotations = masks
-    seg = fast_process(
-        annotations=annotations,
-        image=image,
-        device=device,
-        scale=(1024 // input_size),
-        better_quality=better_quality,
-        mask_random_color=mask_random_color,
-        bbox=None,
-        use_retina=use_retina,
-        withContours=withContours,
-    )
-
-    return seg
-
-
 img_p = gr.Image(label="Input with points", type="pil")
 img_b = gr.Image(label="Input with box", type="pil")
-img_e = gr.Image(label="Input (everything)", type="pil")
-
-if args.enable_everything_mode:
-    all_outputs = [img_p, img_b, img_e]
-else:
-    all_outputs = [img_p, img_b]
 
 with gr.Blocks(css=css, title="EdgeSAM") as demo:
     with gr.Row():
@@ -411,29 +393,6 @@ with gr.Blocks(css=css, title="EdgeSAM") as demo:
                     run_on_click=True
                 )
 
-    if args.enable_everything_mode:
-        with gr.Tab("Everything mode") as tab_e:
-            # Images
-            with gr.Row(variant="panel"):
-                with gr.Column(scale=1):
-                    img_e.render()
-                with gr.Column(scale=1):
-                    with gr.Row():
-                        with gr.Column():
-                            segment_btn_e = gr.Button("Start", variant="primary")
-                            reset_btn_e = gr.Button("Reset", variant="secondary")
-                            gr.Markdown(description_e)
-
-            # Submit & Clear
-            with gr.Row():
-                with gr.Column():
-                    gr.Markdown("Try some of the examples below ⬇️")
-                    gr.Examples(
-                        examples=examples,
-                        inputs=[img_e],
-                        examples_per_page=8,
-                    )
-
     # with gr.Row():
     #     with gr.Column(scale=1):
     #         gr.Markdown(
@@ -444,21 +403,14 @@ with gr.Blocks(css=css, title="EdgeSAM") as demo:
 
     clear_btn_p.click(clear, outputs=[img_p])
     reset_btn_p.click(reset, outputs=[img_p])
-    tab_p.select(fn=reset_all, outputs=all_outputs)
+    tab_p.select(fn=reset_all, outputs=[img_p, img_b])
 
     img_b.upload(on_image_upload, img_b, [img_b])
     img_b.select(segment_with_box, outputs=[img_b])
 
     clear_btn_b.click(clear, outputs=[img_b])
     reset_btn_b.click(reset, outputs=[img_b])
-    tab_b.select(fn=reset_all, outputs=all_outputs)
-
-    if args.enable_everything_mode:
-        segment_btn_e.click(
-            segment_everything, inputs=[img_e], outputs=img_e
-        )
-        reset_btn_e.click(reset, outputs=[img_e])
-        tab_e.select(fn=reset_all, outputs=all_outputs)
+    tab_b.select(fn=reset_all, outputs=[img_p, img_b])
 
 demo.queue()
 demo.launch(server_name=args.server_name, server_port=args.port)
